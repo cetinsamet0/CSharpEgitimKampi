d239b63ba26e718090f8ca59f6c471346a956bad
HP <HP@DESKTOP-3LI12ND>
2024-12-04 21:39:51 +0300

    Yeni

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -15,20 +15,67 @@ namespace _04_Loops
             //x başlangıç değeri
             //y bitiş değeri
             //z artış yada azalış miktarı
-            int i;
-            for (i=1; i <= 1782; i++)
-            {
-                Console.WriteLine("Dosya Yükleniyor..." + i + "/1782" );
-                Console.SetWindowSize(32,16);
+            //int i;
+            //for (i=1; i <= 1782; i++)
+            //{
+            //    Console.WriteLine("Dosya Yükleniyor..." + i + "/1782" );
+            //    Console.SetWindowSize(32,16);
 
-            }
+            //}
 
-            Console.WriteLine("Bütün hepsi başarıyla kuruldu!");
+            //Console.WriteLine("Bütün hepsi başarıyla kuruldu!");
 
-            //BreakPoint Kullanımını Öğren 10.Dakika
+            //for(int i = 1; i <=50; i+=3)
+            //{
+            //    Console.WriteLine(i);  
+            //}
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz sayı adedini giriniz: ");
+            //int finishValue = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Lütfen kaçtan başlayacağına karar veriniz: ");
+            //int startValue = int.Parse(Console.ReadLine());
+            //for (int i = startValue; i <= finishValue; i+=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
             #endregion
 
+            #region For Döngüsü İle Karar Yapıları
 
+            //for(int i = 1; i >=-100; i--)
+            //{
+
+            //    if(i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //int totalValue = 0;
+            //for(int i = 1;i<10;i++ )
+            //{
+            //    totalValue+=i;
+            //    Console.WriteLine(totalValue);
+            //}
+
+            //int totalValue = 0;
+            //for(int i=1;i<100;i+=3)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+                   
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+                    
+            //    }
+            //}
+            //Console.WriteLine("-------------");
+            //Console.WriteLine(totalValue);  
+
+            //19.Dakikada Kaldım
+            #endregion
             Console.Read();
         }
     }
